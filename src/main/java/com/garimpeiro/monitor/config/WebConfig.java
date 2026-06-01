89b506ac7b891c1c5b8a6527bc734447d323be16
kubota2003 <matheusyuji85@gmail.com>
2026-06-01 17:58:46 -0300

    Ajustando comentarios de DEV

--- src/main/java/com/garimpeiro/monitor/config/WebConfig.java
+++ src/main/java/com/garimpeiro/monitor/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer{
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**") // Libera todas as rotas da nossa API (itens, categorias, etc)
-                .allowedOrigins("*") // Libera o acesso de QUALQUER site ou porta (ideal para desenvolvimento)
+        registry.addMapping("/**") // Libera todas as rotas da API 
+                .allowedOrigins("*") // Libera o acesso de QUALQUER site ou porta 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*"); // Libera todos os tipos de cabeçalhos
     }
